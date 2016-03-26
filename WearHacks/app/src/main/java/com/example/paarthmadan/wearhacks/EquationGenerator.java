c34cc08b1678712ee2fdeb92dbbee9f5c713c108
paarthmadan <madanhockey@hotmail.com>
2016-03-26 07:37:47 -0400

    case 3, 4

--- WearHacks/app/src/main/java/com/example/paarthmadan/wearhacks/EquationGenerator.java
+++ WearHacks/app/src/main/java/com/example/paarthmadan/wearhacks/EquationGenerator.java
@@ -53,14 +53,79 @@ public class EquationGenerator {
 
                     System.out.println(firstNum + " " + operator + " " + secondNum);
 
-                    
+
                 }
 
 
                 break;
             case "three":
+
+                for(int i = 0; i <= QUESTIONS_PER_LEVEL; i++){
+
+                    int firstNum = (int)(Math.random() * 90) + 10;
+                    int operatorBool = (int)((Math.random() * 2) + 1);
+                    String operator = "";
+
+                    if(operatorBool == 1){
+                        operator = "*";
+                    }else if(operatorBool == 2){
+                        operator = "/";
+                    }
+
+                    boolean isSecondDivisbleByFirst = false;
+
+                    int secondNum = 0;
+
+                    while(!isSecondDivisbleByFirst) {
+
+                        secondNum = (int) (Math.random() * 9) + 1;
+
+                        if(firstNum % secondNum == 0) {
+                            isSecondDivisbleByFirst=true;
+                        }
+
+
+
+                    }
+
+                    System.out.println(firstNum + " " + operator + " " + secondNum);
+
+                }
+
+
                 break;
             case "four":
+
+                for(int i = 0; i <= QUESTIONS_PER_LEVEL; i++){
+
+                    int firstNum = (int)(Math.random() * 900) + 100;
+                    int operatorBool = (int)((Math.random() * 2) + 1);
+                    String operator = "";
+
+                    if(operatorBool == 1){
+                        operator = "+";
+                    }else if(operatorBool == 2){
+                        operator = "-";
+                    }
+
+                    int secondNum = (int)(Math.random() * 900) + 100;
+
+
+
+                    if(secondNum > firstNum){
+                        int temp = firstNum;
+
+                        firstNum = secondNum;
+
+                        secondNum = temp;
+                    }
+
+                    System.out.println(firstNum + " " + operator + " " + secondNum);
+
+
+                }
+
+
                 break;
             case "five":
                 break;
