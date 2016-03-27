de390cecf03d6574c2e6b708fd06582898401e26
paarthmadan <madanhockey@hotmail.com>
2016-03-27 11:02:14 -0400

    basketball animationn with jquery, and detection

--- Leap Motion/assets/game.js
+++ Leap Motion/assets/game.js
@@ -1,22 +1,21 @@
- 
-
-
+ $(document).ready(function(){
  var canvas = document.getElementById("theCanvas");
       var ct = canvas.getContext("2d");
       var x = canvas.width/2;
-      var y = canvas.height/2 - 75;
+      var y = canvas.height/2 +200;
       var dy= 0;
       var randomX;
 
+      var SPEED_OF_ANIMATION = 2000;
 
       var TIME_OF_CALL = 2000;
 
-      var img=document.getElementById("cartoonImg");
+      var img = document.getElementById("cartoonImg");
       var ball = document.getElementById("cartoonBallImg");
 
 
       
-
+      var counter = 0;
 
       var FRAMES_PER_SECOND = 50;
       var RATIO_CANVAS_TO_LEAP = 2.45;
@@ -36,31 +35,22 @@
           y = 1200;
         }
 
-        ct.drawImage(cartoonImg, x, y);
+        ct.drawImage(img, x, y);
         
       }
 
 
-      function spawnBall(){
+      /*function spawnBall(){
 
+        ct.drawImage(ball,randomX, dy);
 
-
-        
-
-
-        ct.drawImage(cartoonBallImg,randomX, dy);
-
-      }
+      }*/
 
 
 
       function draw(){
         ct.clearRect(0,0,canvas.width, canvas.height); 
         moveObj();
-        
-        console.log(dy)
-
-
 
           Leap.loop({enableGestures: true}, function(frame) {
 
@@ -77,20 +67,98 @@
           });
       }
 
+
       setInterval(draw, FRAMES_PER_SECOND);
 
 
-window.setInterval(function(){
+
 
 randomX = Math.floor((Math.random() * canvas.width) + 1);
 
+// var tid2 = setTimeout(larger,1000);
 
-    window.setInterval(function(){
-  spawnBall(dy += 10, randomX);
-}, 25);
+var noballs = 1;
+
+
+
+if(noballs == 1){
+        $("#cartoonBallImg").css("top", "0px");
+        $("#cartoonBallImg").css("left", randomX + "px");
+        $("#cartoonBallImg").animate({top: canvas.height/2 +400}, SPEED_OF_ANIMATION);
+
+      }
+
+
+
+
+var time = setInterval(checkPos, 10);
+
+function checkPos(){
+
+        var leftBall = randomX;
+        var topBall = $("#cartoonBallImg").offset().top;
+
+        var leftHoop = x;
+        var topHoop = $("#cartoonImg").offset().top;
+
+        
+        console.log(leftBall + "     "+leftHoop);
+        
+        if (Math.abs((leftHoop)-(leftBall))<50 && Math.abs(topHoop-topBall) < 250){
+            counter++;
+
+            if(counter == 1){
+            alert("success" + leftHoop + " "+ leftBall);
+          }
+        }
+
+
+}      
+
+
+
+
+
+/*function larger(){
+
+    var tid = setTimeout(mycode,5);
+
+    function mycode(){
+
+
+
+      if(nooballs == 1){
+        $("#cartoonBallImg").css("top", "0px");
+        $("#cartoonBallImg").css("left",randomX);
+      }
+        
+      /*spawnBall(dy += 1, randomX);
+                  
+                  if (Math.abs(randomX-x)<60 && Math.abs(dy-y) < 60){
+
+                    console.log("pass");
+                    }
+
+    tid = setTimeout(mycode, 5); 
+
+
+        if(dy > 1000){
+          dy = 0;
+        }
+
+    }
+
+
+    tid2 = setTimeout(larger, 1000);
+
+
+}*/
+});
+
+
 
   
-}, 2500);
+
 
 
 
