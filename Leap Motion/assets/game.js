b969022fe4027657e51ed5d8c6a25b6fe2dd2a37
paarthmadan <madanhockey@hotmail.com>
2016-03-26 22:17:06 -0400

    MAJOR, added basketball activity UI

--- Leap Motion/assets/game.js
+++ Leap Motion/assets/game.js
@@ -1,16 +1,33 @@
 	  var canvas = document.getElementById("theCanvas");
       var ct = canvas.getContext("2d");
       var x = canvas.width/2;
-      var y = canvas.height/2;
+      var y = canvas.height/2 - 75;
+
+      var img=document.getElementById("cartoonImg");
+
+    var FRAMES_PER_SECOND = 50;
+    var RATIO_CANVAS_TO_LEAP = 2.45;
+    var OFFSET = 350;
+
+
       ballRadius = 10;
     
 
       function drawObj(){
-      	ct.beginPath();
-      	ct.arc(x,y, ballRadius, 0 , Math.PI*2)
-      	ct.fillStyle= "#FFF"
-      	ct.fill()
-      	ct.closePath();
+        if(x < 0){
+          x = 0;
+        }else if(x > 1100){
+          x = 1100;
+        }
+
+        if(y < 20){
+          y = 20;
+        }else if(y > 1200){
+          y = 1200;
+        }
+
+        ct.drawImage(cartoonImg, x, y);
+      	
       }
 
       function draw(){
@@ -28,18 +45,16 @@
           hand = frame.hands.length
 
 
-           var dataY = (normalized[1] * 1000);
-          // document.getElementById("text").innerHTML = dataY;
+           //var dataY = (normalized[1] * 1000);
 
-           y = (dataY / 2.45) - 350;
+           //y = (dataY / RATIO_CANVAS_TO_LEAP) - OFFSET;
 
-           y *= -1;
-           var dataX = (normalized[0]* 900)
-           // document.getElementById("text2").innerHTML = dataX;
+           //y *= -1;
+           var dataX = (normalized[0]* 900);
 
            x=dataX
 
        });
       })
   }
-      setInterval(draw, 50)
+      setInterval(draw, FRAMES_PER_SECOND);
